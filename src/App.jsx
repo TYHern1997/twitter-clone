2e88bb5e0ea8735eccbc87b3e1ac9ee989414fe5
Henry Tai <tyhern1997@gmail.com>
2026-06-26 11:23:52 +0800

    new-features

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import AuthPage from "./pages/AuthPage";
 import ProfilePage from "./pages/ProfilePage"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { PostsProvider } from "./context/PostsContext";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/profile" element={<ProfilePage />}></Route>
-        <Route path="/login" element={<AuthPage />}></Route>
-        <Route path="*" element={<AuthPage />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <PostsProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/profile" element={<ProfilePage />}></Route>
+          <Route path="/login" element={<AuthPage />}></Route>
+          <Route path="*" element={<AuthPage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </PostsProvider>
   )
 }
